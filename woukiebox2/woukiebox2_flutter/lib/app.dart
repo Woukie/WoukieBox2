edafd1b5cef687d72447879fc8840daeac140a82
Woukie <lucaseldon@live.com>
2024-05-19 17:04:34 +0100

    Initial messages layout

--- woukiebox2/woukiebox2_flutter/lib/app.dart
+++ woukiebox2/woukiebox2_flutter/lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class App extends StatelessWidget {
   final TextStyle? style;
@@ -7,9 +8,37 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      "welcome to the app!",
-      style: style,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('WoukieBox2'),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          tooltip: 'Back to login',
+          onPressed: () {},
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.account_circle_outlined),
+            tooltip: 'User Settings',
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(8.0),
+        color: Colors.amber,
+        child: Row(
+          children: [
+            Expanded(
+              child: Container(
+                color: Colors.deepPurple,
+                child: const Text("messages"),
+              ),
+            ),
+            const Text("users"),
+          ],
+        ),
+      ),
     );
   }
 }
