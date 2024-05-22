5766d178ef75e88d049a3ce0782fe1eea2077d95
Woukie <lucaseldon@live.com>
2024-05-22 15:14:12 +0100

    Fixed stream listenting

--- woukiebox2/woukiebox2_flutter/lib/src/providers/connection_state_provider.dart
+++ woukiebox2/woukiebox2_flutter/lib/src/providers/connection_state_provider.dart
@@ -1,26 +1,48 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:woukiebox2_client/woukiebox2_client.dart';
 import 'package:woukiebox2_flutter/main.dart';
 
 class ConnectionStateProvider extends ChangeNotifier {
   ConnectionState _state = ConnectionState.none;
+  final List<User> _users = List.empty();
+  final List<String> _messages = List.empty(growable: true);
 
   ConnectionState get state => _state;
+  List<User> get users => _users;
+  List<String> get messages => _messages;
+
+  StreamSubscription? _streamSubscription;
 
   void openConnection() async {
     _state = ConnectionState.waiting;
     notifyListeners();
     try {
       await client.openStreamingConnection();
+
       _state = ConnectionState.active;
+
+      _streamSubscription ??= client.sockets.stream.listen(_handleMessage);
     } catch (error) {
+      print(error);
       _state = ConnectionState.none;
     }
     notifyListeners();
   }
 
   Future<void> closeConnection() async {
+    // await _streamSubscription!.cancel();
+    await client.closeStreamingConnection();
     _state = ConnectionState.none;
     notifyListeners();
-    await client.closeStreamingConnection();
+  }
+
+  void _handleMessage(SerializableEntity message) {
+    if (message is ChatMessage) {
+      _messages.add(message.message);
+      print(_messages);
+      notifyListeners();
+    }
   }
 }
