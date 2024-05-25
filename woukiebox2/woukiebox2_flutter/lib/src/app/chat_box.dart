ce38993ef1467ebd706c21b3bc54a5c93e0c8f05
Woukie <lucaseldon@live.com>
2024-05-25 16:55:35 +0100

    Removed unused imports

--- woukiebox2/woukiebox2_flutter/lib/src/app/chat_box.dart
+++ woukiebox2/woukiebox2_flutter/lib/src/app/chat_box.dart
@@ -1,7 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
 import 'package:woukiebox2_client/woukiebox2_client.dart';
 import 'package:woukiebox2_flutter/main.dart';
