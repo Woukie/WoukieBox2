21d05a1f676ba40ae023851372de2d27f52ed9b8
Woukie <lucaseldon@live.com>
2024-05-28 21:27:06 +0100

    Removed unused import

--- woukiebox2/woukiebox2_flutter/lib/src/app/profile_page.dart
+++ woukiebox2/woukiebox2_flutter/lib/src/app/profile_page.dart
@@ -1,4 +1,3 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flex_color_picker/flex_color_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
