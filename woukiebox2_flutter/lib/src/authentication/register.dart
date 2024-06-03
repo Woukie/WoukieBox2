1c76bc87e43541b7fa44c07a6ce796036b5e1f4d
Woukie <lucaseldon@live.com>
2024-06-03 04:32:50 +0100

    Basic register and verifying

--- woukiebox2_flutter/lib/src/authentication/register.dart
+++ woukiebox2_flutter/lib/src/authentication/register.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:serverpod_auth_email_flutter/serverpod_auth_email_flutter.dart';
+import 'package:woukiebox2/main.dart';
 
 class Register extends StatefulWidget {
   const Register({
@@ -10,6 +12,22 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+  bool _enabled = true;
+
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _validationCodeController =
+      TextEditingController();
+
+  late final EmailAuthController _emailAuth;
+
+  @override
+  void initState() {
+    super.initState();
+    _emailAuth = EmailAuthController(client.modules.auth);
+  }
+
   @override
   Widget build(BuildContext context) {
     final TabController tabController = DefaultTabController.of(context);
@@ -19,22 +37,29 @@ class _RegisterState extends State<Register> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          const TextField(
-            decoration: InputDecoration(
+          TextField(
+            enabled: _enabled,
+            controller: _usernameController,
+            decoration: const InputDecoration(
               labelText: "Username",
               border: OutlineInputBorder(),
             ),
           ),
           const Padding(padding: EdgeInsets.all(6)),
-          const TextField(
-            decoration: InputDecoration(
+          TextField(
+            enabled: _enabled,
+            controller: _emailController,
+            decoration: const InputDecoration(
               labelText: "Email",
               border: OutlineInputBorder(),
             ),
           ),
           const Padding(padding: EdgeInsets.all(6)),
-          const TextField(
-            decoration: InputDecoration(
+          TextField(
+            enabled: _enabled,
+            obscureText: true,
+            controller: _passwordController,
+            decoration: const InputDecoration(
               suffixIcon: Icon(Icons.remove_red_eye),
               labelText: "Password",
               border: OutlineInputBorder(),
@@ -45,7 +70,7 @@ class _RegisterState extends State<Register> {
             children: [
               Expanded(
                 child: FilledButton(
-                  onPressed: () {},
+                  onPressed: _enabled ? _createAccount : null,
                   child: const Text("Sign up"),
                 ),
               ),
@@ -59,9 +84,11 @@ class _RegisterState extends State<Register> {
             children: [
               const Text("Already have an account? "),
               TextButton(
-                onPressed: () {
-                  tabController.index = 0;
-                },
+                onPressed: _enabled
+                    ? () {
+                        tabController.index = 0;
+                      }
+                    : null,
                 style: const ButtonStyle(),
                 child: const Text("Login Now"),
               )
@@ -70,5 +97,101 @@ class _RegisterState extends State<Register> {
         ],
       ),
     );
+  }
+
+  Future<void> _createAccount() async {
+    setState(() {
+      _enabled = false;
+    });
+
+    var userName = _usernameController.text.trim();
+    var email = _emailController.text.trim().toLowerCase();
+    var password = _passwordController.text;
+
+    var success = await _emailAuth.createAccountRequest(
+      userName,
+      email,
+      password,
+    );
+
+    setState(() {
+      _enabled = true;
+
+      if (success) {
+        _confirmEmailDialoge(context);
+      }
+    });
+  }
+
+  Future<void> _confirmEmailDialoge(BuildContext context) {
+    final TextEditingController _validationCodeController =
+        TextEditingController();
+
+    return showDialog<void>(
+      barrierDismissible: false,
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(8.0)),
+          ),
+          contentPadding: const EdgeInsets.all(8),
+          actionsPadding: const EdgeInsets.only(bottom: 8, right: 8),
+          content: TextField(
+            enabled: _enabled,
+            controller: _validationCodeController,
+            decoration: const InputDecoration(
+              labelText: 'Verification code',
+              helperText: 'Enter the code we sent to your inbox',
+              border: OutlineInputBorder(),
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: Theme.of(context).textTheme.labelLarge,
+              ),
+              onPressed: _enabled
+                  ? () {
+                      Navigator.of(context).pop();
+                    }
+                  : null,
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: Theme.of(context).textTheme.labelLarge,
+              ),
+              onPressed: _enabled ? _validateAccount : null,
+              child: const Text('Verify'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<void> _validateAccount() async {
+    var email = _emailController.text.toLowerCase().trim();
+
+    setState(() {
+      _enabled = false;
+    });
+
+    var userInfo = await _emailAuth.validateAccount(
+      email,
+      _validationCodeController.text,
+    );
+
+    var result = await _emailAuth.signIn(email, _passwordController.text);
+    if (result == null) {
+      setState(() {
+        _enabled = true;
+      });
+    }
+
+    if (mounted) {
+      Navigator.of(context).pop();
+    }
   }
 }
