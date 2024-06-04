a3aa18fc14db9c2e85a046d830f8c361eb03ef8e
Woukie <lucaseldon@live.com>
2024-06-04 23:17:22 +0100

    Further refactoring and removal of unused code

--- woukiebox2_flutter/lib/src/app/app.dart
+++ woukiebox2_flutter/lib/src/app/app.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:woukiebox2/src/app/main_app_bar.dart';
 
-import 'chatroom/message_box/message_box.dart';
-import 'chatroom/messages/messages.dart';
-import 'chatroom/users/users.dart';
+import 'chatroom/chat_room.dart';
 
 class App extends StatefulWidget {
   const App({
@@ -21,44 +19,10 @@ class _AppState extends State<App> {
       elevation: 0.5,
       shape: Border.all(width: 0, color: Colors.transparent),
       margin: const EdgeInsets.all(0),
-      child: Column(
+      child: const Column(
         children: [
-          const MainAppBar(),
-          Expanded(
-            child: Container(
-              padding: const EdgeInsets.all(12),
-              child: Column(
-                children: [
-                  Expanded(
-                    child: Row(
-                      children: [
-                        const Expanded(
-                          child: Card(
-                            margin: EdgeInsets.all(0.0),
-                            elevation: 0,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [
-                                Flexible(
-                                  child: Messages(),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                        Container(
-                          width: 200,
-                          alignment: Alignment.topLeft,
-                          child: const Users(),
-                        ),
-                      ],
-                    ),
-                  ),
-                  const MessageBox()
-                ],
-              ),
-            ),
-          ),
+          MainAppBar(),
+          ChatRoom(),
         ],
       ),
     );
---
+++ woukiebox2_flutter/lib/src/app/chatroom/chat_room.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:woukiebox2/src/app/chatroom/message_box/message_box.dart';
+import 'package:woukiebox2/src/app/chatroom/messages/messages.dart';
+import 'package:woukiebox2/src/app/chatroom/users/users.dart';
+
+class ChatRoom extends StatelessWidget {
+  const ChatRoom({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          children: [
+            Expanded(
+              child: Row(
+                children: [
+                  const Expanded(
+                    child: Card(
+                      margin: EdgeInsets.all(0.0),
+                      elevation: 0,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Flexible(
+                            child: Messages(),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 200,
+                    alignment: Alignment.topLeft,
+                    child: const Users(),
+                  ),
+                ],
+              ),
+            ),
+            const MessageBox()
+          ],
+        ),
+      ),
+    );
+  }
+}
