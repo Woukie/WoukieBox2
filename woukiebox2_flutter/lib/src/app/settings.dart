63520dce7a16c47b885d9116e48f18181a3cd2c6
Woukie <lucaseldon@live.com>
2024-06-05 01:16:14 +0100

    Added log out button

--- woukiebox2_flutter/lib/src/app/settings.dart
+++ woukiebox2_flutter/lib/src/app/settings.dart
@@ -1,6 +1,7 @@
 import 'package:flex_color_picker/flex_color_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:woukiebox2/src/providers/connection_state_provider.dart';
 import 'package:woukiebox2/src/providers/preference_provider.dart';
 
 class Settings extends StatelessWidget {
@@ -10,17 +11,18 @@ class Settings extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final connectionStateProvider =
+        Provider.of<ConnectionStateProvider>(context);
     final preferenceProvider = Provider.of<PreferenceProvider>(context);
 
-    return Container(
-      width: 500,
-      padding: const EdgeInsets.all(12),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(bottom: 8),
-            child: Row(
+    return ConstrainedBox(
+      constraints: const BoxConstraints.expand(width: 700),
+      child: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
                 Padding(
@@ -34,36 +36,35 @@ class Settings extends StatelessWidget {
                 const Text("Dark Mode"),
               ],
             ),
-          ),
-          FilledButton(
-              onPressed: () {
-                Navigator.of(context).pop();
+            FilledButton.tonalIcon(
+              icon: const Icon(Icons.edit),
+              onPressed: () async {
+                preferenceProvider.updateSelectedColor(
+                  await showColorPickerDialog(
+                    context,
+                    preferenceProvider.color,
+                    pickersEnabled: <ColorPickerType, bool>{
+                      ColorPickerType.accent: false,
+                      ColorPickerType.primary: true,
+                    },
+                    enableShadesSelection: false,
+                  ),
+                );
               },
-              child: const Text("push")),
-          Row(
-            children: [
-              Expanded(
-                child: FilledButton.tonalIcon(
-                  icon: const Icon(Icons.edit),
-                  onPressed: () async {
-                    preferenceProvider.updateSelectedColor(
-                      await showColorPickerDialog(
-                        context,
-                        preferenceProvider.color,
-                        pickersEnabled: <ColorPickerType, bool>{
-                          ColorPickerType.accent: false,
-                          ColorPickerType.primary: true,
-                        },
-                        enableShadesSelection: false,
-                      ),
-                    );
-                  },
-                  label: const Text("Theme Colour"),
-                ),
-              ),
-            ],
-          ),
-        ],
+              label: const Text("Theme Colour"),
+            ),
+            FilledButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text("Fuck my whole shit up")),
+            FilledButton(
+                onPressed: () {
+                  connectionStateProvider.closeConnection();
+                },
+                child: const Text("Log out")),
+          ],
+        ),
       ),
     );
   }
