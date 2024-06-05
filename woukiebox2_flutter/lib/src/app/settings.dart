7c754832dc12e56a20fa5c84701b92b8fe80c4c2
Woukie <lucaseldon@live.com>
2024-06-05 05:46:38 +0100

    Added card to settings

--- woukiebox2_flutter/lib/src/app/settings.dart
+++ woukiebox2_flutter/lib/src/app/settings.dart
@@ -1,5 +1,6 @@
 import 'package:flex_color_picker/flex_color_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
 import 'package:woukiebox2/src/providers/connection_state_provider.dart';
 import 'package:woukiebox2/src/providers/preference_provider.dart';
@@ -17,117 +18,131 @@ class Settings extends StatelessWidget {
 
     return ConstrainedBox(
       constraints: const BoxConstraints.expand(width: 700),
-      child: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(12),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text(
-                "Appearance",
-                style: TextStyle(fontSize: 22),
-              ),
+      child: Column(
+        children: [
+          Expanded(
+            child: Card(
+              margin: const EdgeInsets.all(12),
+              elevation: 0,
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        "Appearance",
+                        style: TextStyle(fontSize: 22),
+                      ),
 
-              // Dark Mode
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Text("Dark Mode"),
-                  Switch(
-                    value: preferenceProvider.themeMode == ThemeMode.dark,
-                    onChanged: (value) =>
-                        preferenceProvider.toggleThemeMode(), // Toggle theme
-                  ),
-                ],
-              ),
-              const Text("Toggle between light and dark mode"),
-              const Divider(),
+                      // Dark Mode
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text("Dark Mode"),
+                          Switch(
+                            value:
+                                preferenceProvider.themeMode == ThemeMode.dark,
+                            onChanged: (value) => preferenceProvider
+                                .toggleThemeMode(), // Toggle theme
+                          ),
+                        ],
+                      ),
+                      const Text("Toggle between light and dark mode"),
+                      const Divider(),
 
-              // Theme Color
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Text("Theme Color"),
-                  FilledButton.tonalIcon(
-                    label: const Text("Edit"),
-                    icon: const Icon(Icons.edit),
-                    onPressed: () async {
-                      preferenceProvider.updateSelectedColor(
-                        await showColorPickerDialog(
-                          context,
-                          preferenceProvider.color,
-                          pickersEnabled: <ColorPickerType, bool>{
-                            ColorPickerType.accent: false,
-                            ColorPickerType.primary: true,
+                      // Theme Color
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text("Theme Color"),
+                          FilledButton.tonalIcon(
+                            label: const Text("Edit"),
+                            icon: const Icon(Icons.edit),
+                            onPressed: () async {
+                              preferenceProvider.updateSelectedColor(
+                                await showColorPickerDialog(
+                                  context,
+                                  preferenceProvider.color,
+                                  pickersEnabled: <ColorPickerType, bool>{
+                                    ColorPickerType.accent: false,
+                                    ColorPickerType.primary: true,
+                                  },
+                                  enableShadesSelection: false,
+                                ),
+                              );
+                            },
+                          ),
+                        ],
+                      ),
+                      const Text(
+                          "Select a color to be applied to the styling of the app"),
+                      const Divider(),
+
+                      const Padding(padding: EdgeInsets.all(12)),
+                      const Text(
+                        "Notifications",
+                        style: TextStyle(fontSize: 22),
+                      ),
+
+                      // Toggle Desktop Notifications
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text("Desktop Notifications"),
+                          Switch(
+                            value:
+                                preferenceProvider.themeMode == ThemeMode.dark,
+                            onChanged: (value) => preferenceProvider
+                                .toggleThemeMode(), // Toggle theme
+                          ),
+                        ],
+                      ),
+                      const Text(
+                          "Enable desktop notifications for unread messages"),
+                      const Divider(),
+
+                      // Toggle Desktop Notifications
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text("Taskbar Flashing"),
+                          Switch(
+                            value:
+                                preferenceProvider.themeMode == ThemeMode.dark,
+                            onChanged: (value) => preferenceProvider
+                                .toggleThemeMode(), // Toggle theme
+                          ),
+                        ],
+                      ),
+                      const Text(
+                          "Enables flashing of the taskbar icon for unread messages"),
+                      const Divider(),
+
+                      const Padding(padding: EdgeInsets.all(12)),
+                      const Text(
+                        "Misc",
+                        style: TextStyle(fontSize: 22),
+                      ),
+
+                      FilledButton(
+                          onPressed: () {
+                            connectionStateProvider.closeConnection();
                           },
-                          enableShadesSelection: false,
-                        ),
-                      );
-                    },
+                          child: const Text("Log out")),
+
+                      FilledButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: const Text("Fuck my whole shit up")),
+                    ],
                   ),
-                ],
+                ),
               ),
-              const Text(
-                  "Select a color to be applied to the styling of the app"),
-              const Divider(),
-
-              const Padding(padding: EdgeInsets.all(12)),
-              const Text(
-                "Notifications",
-                style: TextStyle(fontSize: 22),
-              ),
-
-              // Toggle Desktop Notifications
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Text("Desktop Notifications"),
-                  Switch(
-                    value: preferenceProvider.themeMode == ThemeMode.dark,
-                    onChanged: (value) =>
-                        preferenceProvider.toggleThemeMode(), // Toggle theme
-                  ),
-                ],
-              ),
-              const Text("Enable desktop notifications for unread messages"),
-              const Divider(),
-
-              // Toggle Desktop Notifications
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Text("Taskbar Flashing"),
-                  Switch(
-                    value: preferenceProvider.themeMode == ThemeMode.dark,
-                    onChanged: (value) =>
-                        preferenceProvider.toggleThemeMode(), // Toggle theme
-                  ),
-                ],
-              ),
-              const Text(
-                  "Enables flashing of the app icon on your taskbar when you get an unread message"),
-              const Divider(),
-
-              const Padding(padding: EdgeInsets.all(12)),
-              const Text(
-                "Misc",
-                style: TextStyle(fontSize: 22),
-              ),
-
-              FilledButton(
-                  onPressed: () {
-                    connectionStateProvider.closeConnection();
-                  },
-                  child: const Text("Log out")),
-
-              FilledButton(
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                  child: const Text("Fuck my whole shit up")),
-            ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
