7fd2a274933d895282261db8e15dee9a14d06795
Woukie <lucaseldon@live.com>
2024-06-07 13:03:02 +0100

    Display friend in friend list when user is available

---
+++ woukiebox2_flutter/lib/src/app/friends/friend.dart
@@ -0,0 +1,95 @@
+import 'package:flex_color_picker/flex_color_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:woukiebox2/main.dart';
+import 'package:woukiebox2/src/app/profile/profile_pic.dart';
+import 'package:woukiebox2/src/providers/app_state_provider.dart';
+import 'package:woukiebox2/src/util/hex_color.dart';
+import 'package:woukiebox2_client/woukiebox2_client.dart';
+
+class Friend extends StatelessWidget {
+  const Friend({
+    super.key,
+    required this.userId,
+    this.showNegative = true,
+    this.showPositive = true,
+    this.positiveIcon = Icons.person_add,
+    this.negativeIcon = Icons.person_remove,
+  });
+
+  final bool showNegative;
+  final bool showPositive;
+  final IconData positiveIcon;
+  final IconData negativeIcon;
+  final int userId;
+
+  @override
+  Widget build(BuildContext context) {
+    AppStateProvider appStateProvider = Provider.of<AppStateProvider>(context);
+
+    User user = appStateProvider.users[userId] ??
+        User(
+          id: userId,
+          username: "Loading...",
+          bio: "Loading...",
+          colour:
+              (Theme.of(context).textTheme.labelMedium?.color ?? Colors.white)
+                  .hex,
+          image: "",
+          verified: true,
+        );
+
+    return Padding(
+      padding: const EdgeInsets.only(right: 12, top: 12),
+      child: Card(
+        color: Theme.of(context).colorScheme.surfaceContainer,
+        elevation: .5,
+        margin: EdgeInsets.zero,
+        child: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(12),
+              child: ProfilePic(
+                url: user.image,
+              ),
+            ),
+            Expanded(
+              child: Text(
+                user.username,
+                style: TextStyle(color: HexColor.fromHex(user.colour)),
+                overflow: TextOverflow.fade,
+              ),
+            ),
+            showNegative
+                ? Padding(
+                    padding: const EdgeInsets.only(left: 12),
+                    child: IconButton.filledTonal(
+                      onPressed: () {
+                        client.sockets.sendStreamMessage(
+                          FriendRequest(target: userId, positive: false),
+                        );
+                      },
+                      icon: Icon(negativeIcon),
+                    ),
+                  )
+                : Container(),
+            showPositive
+                ? Padding(
+                    padding: const EdgeInsets.only(left: 12),
+                    child: IconButton.filled(
+                      onPressed: () {
+                        client.sockets.sendStreamMessage(
+                          FriendRequest(target: userId, positive: true),
+                        );
+                      },
+                      icon: Icon(positiveIcon),
+                    ),
+                  )
+                : Container(),
+            const Padding(padding: EdgeInsets.only(right: 12)),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- woukiebox2_flutter/lib/src/app/friends/friends.dart
+++ woukiebox2_flutter/lib/src/app/friends/friends.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:woukiebox2/main.dart';
-import 'package:woukiebox2/src/app/profile/profile_pic.dart';
 import 'package:woukiebox2/src/providers/app_state_provider.dart';
-import 'package:woukiebox2_client/woukiebox2_client.dart';
+
+import 'friend.dart';
 
 class Friends extends StatefulWidget {
   const Friends({
@@ -126,79 +125,6 @@ class FriendList extends StatelessWidget {
           negativeIcon: negativeIcon,
         );
       },
-    );
-  }
-}
-
-class Friend extends StatelessWidget {
-  const Friend({
-    super.key,
-    required this.userId,
-    this.showNegative = true,
-    this.showPositive = true,
-    this.positiveIcon = Icons.person_add,
-    this.negativeIcon = Icons.person_remove,
-  });
-
-  final bool showNegative;
-  final bool showPositive;
-  final IconData positiveIcon;
-  final IconData negativeIcon;
-  final int userId;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(right: 12, top: 12),
-      child: Card(
-        color: Theme.of(context).colorScheme.surfaceContainer,
-        elevation: .5,
-        margin: EdgeInsets.zero,
-        child: Row(
-          children: [
-            const Padding(
-              padding: EdgeInsets.all(12),
-              child: ProfilePic(
-                url:
-                    "https://www.gannett-cdn.com/-mm-/968b2fdbf02baeb86111b3ffda77ca1822b5d9ba/c=0-875-1854-2269&r=x513&c=680x510/local/-/media/USATODAY/USATODAY/2014/09/09/1410265931002-911aniv14_002.jpg",
-              ),
-            ),
-            Expanded(
-              child: Text(
-                "$userId",
-                overflow: TextOverflow.fade,
-              ),
-            ),
-            showNegative
-                ? Padding(
-                    padding: const EdgeInsets.only(left: 12),
-                    child: IconButton.filledTonal(
-                      onPressed: () {
-                        client.sockets.sendStreamMessage(
-                          FriendRequest(target: userId, positive: false),
-                        );
-                      },
-                      icon: Icon(negativeIcon),
-                    ),
-                  )
-                : Container(),
-            showPositive
-                ? Padding(
-                    padding: const EdgeInsets.only(left: 12),
-                    child: IconButton.filled(
-                      onPressed: () {
-                        client.sockets.sendStreamMessage(
-                          FriendRequest(target: userId, positive: true),
-                        );
-                      },
-                      icon: Icon(positiveIcon),
-                    ),
-                  )
-                : Container(),
-            const Padding(padding: EdgeInsets.only(right: 12)),
-          ],
-        ),
-      ),
     );
   }
 }
