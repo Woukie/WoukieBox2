3e0f54fb0019aed6c1e82ba7116294c124a34eae
Woukie <lucaseldon@live.com>
2024-06-07 05:41:29 +0100

    Added navigation rail to friend list

--- woukiebox2_flutter/lib/src/app/friends/friends.dart
+++ woukiebox2_flutter/lib/src/app/friends/friends.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
 import 'package:woukiebox2/src/providers/app_state_provider.dart';
 import 'package:woukiebox2/src/providers/preference_provider.dart';
@@ -13,22 +14,47 @@ class Friends extends StatelessWidget {
   Widget build(BuildContext context) {
     final appStateProvider = Provider.of<AppStateProvider>(context);
 
-    return ConstrainedBox(
-      constraints: const BoxConstraints.expand(width: 700),
-      child: Column(
+    return Card(
+      margin: const EdgeInsets.all(12),
+      elevation: 0,
+      child: Row(
         children: [
-          Expanded(
-            child: Card(
-              margin: const EdgeInsets.all(12),
-              elevation: 0,
-              child: ListView.builder(
-                padding: const EdgeInsets.only(bottom: 12),
-                shrinkWrap: true,
-                itemCount: appStateProvider.friends.length,
-                itemBuilder: (context, index) {
-                  return Friend(friendId: index);
-                },
+          NavigationRail(
+            backgroundColor: Theme.of(context).cardColor,
+            selectedIndex: 0,
+            extended: true,
+            destinations: const <NavigationRailDestination>[
+              NavigationRailDestination(
+                icon: Icon(Icons.person_outline),
+                selectedIcon: Icon(Icons.person),
+                label: Text('Friends'),
               ),
+              NavigationRailDestination(
+                icon: Icon(Icons.outbox_outlined),
+                selectedIcon: Icon(Icons.outbox),
+                label: Text("Outgoing"),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.inbox_outlined),
+                selectedIcon: Icon(Icons.inbox),
+                label: Text("Incoming"),
+              ),
+            ],
+          ),
+          Expanded(
+            child: Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                    padding: const EdgeInsets.only(bottom: 12),
+                    shrinkWrap: true,
+                    itemCount: appStateProvider.friends.length,
+                    itemBuilder: (context, index) {
+                      return Friend(friendId: index);
+                    },
+                  ),
+                ),
+              ],
             ),
           ),
         ],
