8940a297311d65c35a55d4672731fb15824e97cc
Woukie <lucaseldon@live.com>
2024-06-07 07:07:26 +0100

    Added buttons to friends

--- woukiebox2_flutter/lib/src/app/friends/friends.dart
+++ woukiebox2_flutter/lib/src/app/friends/friends.dart
@@ -1,4 +1,3 @@
-import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:woukiebox2/src/app/profile/profile_pic.dart';
@@ -65,14 +64,26 @@ class _FriendsState extends State<Friends> {
                 0 => FriendList(
                     key: const Key("friends"),
                     userIds: appStateProvider.friends,
+                    negativeCallback: (userId) {
+                      print(userId);
+                    },
                   ),
                 1 => FriendList(
                     key: const Key("outgoingFriendRequests"),
                     userIds: appStateProvider.outgoingFriendRequests,
+                    negativeCallback: (userId) {
+                      print(userId);
+                    },
                   ),
                 2 => FriendList(
                     key: const Key("incomingFriendRequests"),
                     userIds: appStateProvider.incomingFriendRequests,
+                    positiveCallback: (userId) {
+                      print(userId);
+                    },
+                    negativeCallback: (userId) {
+                      print(userId);
+                    },
                   ),
                 _ => Container(),
               },
@@ -88,25 +99,40 @@ class FriendList extends StatelessWidget {
   const FriendList({
     super.key,
     required this.userIds,
+    this.positiveCallback,
+    this.negativeCallback,
   });
 
+  final Function(int userId)? positiveCallback;
+  final Function(int userId)? negativeCallback;
   final List<int> userIds;
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      prototypeItem: const Friend(userId: -1),
+      prototypeItem: const Friend(userId: 1),
       padding: const EdgeInsets.only(bottom: 12),
       itemCount: userIds.length,
       itemBuilder: (context, index) {
-        return Friend(userId: userIds[index]);
+        return Friend(
+          userId: userIds[index],
+          positiveCallback: positiveCallback,
+          negativeCallback: negativeCallback,
+        );
       },
     );
   }
 }
 
 class Friend extends StatelessWidget {
-  const Friend({super.key, required this.userId});
+  const Friend(
+      {super.key,
+      required this.userId,
+      this.positiveCallback,
+      this.negativeCallback});
+
+  final Function(int userId)? positiveCallback;
+  final Function(int userId)? negativeCallback;
 
   final int userId;
 
@@ -126,7 +152,35 @@ class Friend extends StatelessWidget {
                     "https://www.gannett-cdn.com/-mm-/968b2fdbf02baeb86111b3ffda77ca1822b5d9ba/c=0-875-1854-2269&r=x513&c=680x510/local/-/media/USATODAY/USATODAY/2014/09/09/1410265931002-911aniv14_002.jpg",
               ),
             ),
-            Text("$userId"),
+            Expanded(
+              child: Text(
+                "$userId",
+                overflow: TextOverflow.fade,
+              ),
+            ),
+            negativeCallback != null
+                ? Padding(
+                    padding: const EdgeInsets.only(left: 12),
+                    child: IconButton.filledTonal(
+                      onPressed: () {
+                        negativeCallback!(userId);
+                      },
+                      icon: const Icon(Icons.close),
+                    ),
+                  )
+                : Container(),
+            positiveCallback != null
+                ? Padding(
+                    padding: const EdgeInsets.only(left: 12),
+                    child: IconButton.filled(
+                      onPressed: () {
+                        positiveCallback!(userId);
+                      },
+                      icon: const Icon(Icons.check),
+                    ),
+                  )
+                : Container(),
+            const Padding(padding: EdgeInsets.only(right: 12)),
           ],
         ),
       ),
