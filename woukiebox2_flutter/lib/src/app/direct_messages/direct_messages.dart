4b88d3745d3e6043cac4ef25afd60c639339e18b
Woukie <lucaseldon@live.com>
2024-06-10 10:49:26 +0100

    Initial client "create chat" button

--- woukiebox2_flutter/lib/src/app/direct_messages/direct_messages.dart
+++ woukiebox2_flutter/lib/src/app/direct_messages/direct_messages.dart
@@ -26,84 +26,124 @@ class _DirectMessagesState extends State<DirectMessages> {
 
     return Padding(
       padding: const EdgeInsets.all(12),
-      child: Row(
-        children: [
-          Card(
-            margin: const EdgeInsets.only(right: 12),
-            color: Theme.of(context).colorScheme.surfaceContainerLow,
-            child: SizedBox(
-              width: 200, // Same as minWidth of extended navigation rails
-              child: ListView.builder(
-                padding: const EdgeInsets.fromLTRB(8, 8, 8, 4),
-                itemCount: groupChats.length,
-                itemBuilder: (context, index) {
-                  GroupChat groupChat = groupChats[index];
-                  bool selected = groupChat.id == _selectedGroup;
-
-                  return Padding(
-                    padding: const EdgeInsets.only(bottom: 4),
-                    child: Card(
-                      margin: EdgeInsets.zero,
-                      elevation: selected ? 1 : 0,
-                      color: selected
-                          ? Theme.of(context).colorScheme.surfaceContainer
-                          : null,
-                      child: InkWell(
-                        borderRadius: BorderRadius.circular(12),
-                        onTap: () {
-                          setState(() {
-                            _selectedGroup = groupChat.id;
-                          });
-                        },
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 12,
-                            vertical: 6,
-                          ),
-                          child: Row(
-                            children: [
-                              const ProfilePic(url: ""),
-                              const Padding(
-                                padding: EdgeInsets.only(right: 12),
-                              ),
-                              Expanded(
-                                child: Text(
-                                  softWrap: false,
-                                  groupChat.name,
-                                  overflow: TextOverflow.fade,
-                                ),
-                              ),
-                            ],
-                          ),
+      child: SizedBox(
+        child: Row(
+          children: [
+            Card(
+              margin: const EdgeInsets.only(right: 12),
+              color: Theme.of(context).colorScheme.surfaceContainerLow,
+              child: SizedBox(
+                width: 200,
+                child: Stack(
+                  children: [
+                    ChatsList(
+                      selectedGroup: _selectedGroup,
+                      selectGroup: (selectedGroup) {
+                        setState(() {
+                          _selectedGroup = selectedGroup;
+                        });
+                      },
+                    ),
+                    Align(
+                      alignment: Alignment.topRight,
+                      child: Padding(
+                        padding: const EdgeInsets.all(12),
+                        child: FloatingActionButton.small(
+                          onPressed: () {},
+                          child: const Icon(Icons.add),
                         ),
                       ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: Column(
+                children: [
+                  Expanded(
+                    child: Card(
+                      color: Theme.of(context).colorScheme.surfaceContainerLow,
+                      margin: EdgeInsets.zero,
+                      elevation: 1,
+                      child: _selectedGroup == null
+                          ? Container()
+                          : Messages(
+                              messages: groupChats[_selectedGroup!].messages,
+                            ),
                     ),
-                  );
-                },
+                  ),
+                  MessageBox(target: _selectedGroup ?? 0)
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ChatsList extends StatelessWidget {
+  const ChatsList({
+    super.key,
+    required this.selectedGroup,
+    required this.selectGroup,
+  });
+
+  final int? selectedGroup;
+  final Function(int) selectGroup;
+
+  @override
+  Widget build(BuildContext context) {
+    AppStateProvider appStateProvider = Provider.of<AppStateProvider>(context);
+    final List<GroupChat> groupChats = appStateProvider.chats.values.toList();
+
+    return ListView.builder(
+      padding: const EdgeInsets.fromLTRB(8, 8, 8, 4),
+      itemCount: groupChats.length,
+      itemBuilder: (context, index) {
+        GroupChat groupChat = groupChats[index];
+        bool selected = groupChat.id == selectedGroup;
+
+        return Padding(
+          padding: const EdgeInsets.only(bottom: 4),
+          child: Card(
+            margin: EdgeInsets.zero,
+            elevation: selected ? 1 : 0,
+            color: selected
+                ? Theme.of(context).colorScheme.surfaceContainer
+                : null,
+            child: InkWell(
+              borderRadius: BorderRadius.circular(12),
+              onTap: () {
+                selectGroup(groupChat.id);
+              },
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 12,
+                  vertical: 6,
+                ),
+                child: Row(
+                  children: [
+                    const ProfilePic(url: ""),
+                    const Padding(
+                      padding: EdgeInsets.only(right: 12),
+                    ),
+                    Expanded(
+                      child: Text(
+                        softWrap: false,
+                        groupChat.name,
+                        overflow: TextOverflow.fade,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
-          Expanded(
-            child: Column(
-              children: [
-                Expanded(
-                  child: Card(
-                    color: Theme.of(context).colorScheme.surfaceContainerLow,
-                    margin: EdgeInsets.zero,
-                    elevation: 1,
-                    child: _selectedGroup == null
-                        ? Container()
-                        : Messages(
-                            messages: groupChats[_selectedGroup!].messages,
-                          ),
-                  ),
-                ),
-                MessageBox(target: _selectedGroup ?? 0)
-              ],
-            ),
-          ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
