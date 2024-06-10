6193c658ab2c53e38e8432673619dbc309f77363
Woukie <lucaseldon@live.com>
2024-06-10 12:40:00 +0100

    Implemented add contact list

--- woukiebox2_flutter/lib/src/app/direct_messages/direct_messages.dart
+++ woukiebox2_flutter/lib/src/app/direct_messages/direct_messages.dart
@@ -1,3 +1,5 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:woukiebox2/main.dart';
@@ -6,6 +8,7 @@ import 'package:woukiebox2/src/app/chatroom/messages/messages.dart';
 import 'package:woukiebox2/src/app/profile/profile_pic.dart';
 import 'package:woukiebox2/src/providers/app_state_provider.dart';
 import 'package:woukiebox2/src/util/group_chat.dart';
+import 'package:woukiebox2/src/util/hex_color.dart';
 import 'package:woukiebox2_client/woukiebox2_client.dart';
 
 class DirectMessages extends StatefulWidget {
@@ -52,7 +55,7 @@ class _DirectMessagesState extends State<DirectMessages> {
                         padding: const EdgeInsets.all(12),
                         child: FloatingActionButton.small(
                           onPressed: () {
-                            _createChatDialog(context);
+                            _createFriendSelectionDialog(context);
                           },
                           child: const Icon(Icons.add),
                         ),
@@ -87,52 +90,124 @@ class _DirectMessagesState extends State<DirectMessages> {
     );
   }
 
-  Future<void> _createChatDialog(BuildContext context) {
+  Future<void> _createFriendSelectionDialog(BuildContext context) {
+    HashMap<int, bool> friendsSelection = HashMap<int, bool>();
+
     return showDialog<void>(
       context: context,
       builder: (BuildContext context) {
-        return Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(
-              width: 300,
-              child: Card(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      const Text("Select Contacts"),
-                      const SizedBox(height: 250, child: Text("friends here")),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          TextButton(
-                            style: TextButton.styleFrom(
-                              textStyle: Theme.of(context).textTheme.labelLarge,
-                            ),
-                            child: const Text('Cancel'),
-                            onPressed: () {
-                              Navigator.of(context).pop();
+        AppStateProvider appStateProvider =
+            Provider.of<AppStateProvider>(context);
+
+        List<int> friends = appStateProvider.friends;
+
+        HashMap<int, bool> updatedSelection = HashMap<int, bool>();
+        for (var friend in friends) {
+          updatedSelection[friend] = friendsSelection[friend] ?? false;
+        }
+        friendsSelection = updatedSelection;
+
+        return StatefulBuilder(
+          builder: (context, setState) => Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 300,
+                child: Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const Text("Select Contacts"),
+                        SizedBox(
+                          height: 250,
+                          child: ListView.builder(
+                            itemCount: friends.length,
+                            itemBuilder: (BuildContext context, int index) {
+                              int friendId = friends[index];
+
+                              UserClient? user =
+                                  appStateProvider.users[friendId];
+
+                              if (user == null) {
+                                appStateProvider.scheduleGetUser(friendId);
+                              }
+
+                              return InkWell(
+                                borderRadius: BorderRadius.circular(12),
+                                onTap: () {
+                                  setState(() {
+                                    friendsSelection[friendId] =
+                                        !friendsSelection[friendId]!;
+                                  });
+                                },
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8),
+                                  child: Row(
+                                    children: [
+                                      ProfilePic(
+                                        url: user == null ? "" : user.image,
+                                      ),
+                                      const Padding(
+                                          padding: EdgeInsets.only(right: 8)),
+                                      Expanded(
+                                        child: Text(
+                                          style: TextStyle(
+                                            color: user == null
+                                                ? null
+                                                : HexColor.fromHex(user.colour),
+                                          ),
+                                          user == null
+                                              ? "Loading..."
+                                              : user.username,
+                                        ),
+                                      ),
+                                      const Padding(
+                                          padding: EdgeInsets.only(right: 8)),
+                                      Checkbox(
+                                        value: friendsSelection[friendId],
+                                        onChanged: (value) {
+                                          if (value == null) return;
+                                          setState(() {
+                                            friendsSelection[friendId] = value;
+                                          });
+                                        },
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              );
                             },
                           ),
-                          const Padding(padding: EdgeInsets.only(right: 8)),
-                          FilledButton(
-                            onPressed: () {
-                              Navigator.of(context).pop();
-                            },
-                            child: const Text('Create'),
-                          )
-                        ],
-                      )
-                    ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            TextButton(
+                              child: const Text('Cancel'),
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                            const Padding(padding: EdgeInsets.only(right: 8)),
+                            FilledButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: const Text('Create'),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         );
       },
     );
